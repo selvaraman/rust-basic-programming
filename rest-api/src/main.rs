1056197fe07598bc4fcdb831d4eb3cf64e5e6b79
Selva Raman <selvaraman.sse@gmail.com>
2023-09-24 21:45:00 +0530

    adding logger

--- rest-api/src/main.rs
+++ rest-api/src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{ get, web, App, HttpResponse, HttpServer, Responder, Result };
 use serde::{ Serialize };
+use log::{info};
 
 mod api;
 mod models;
@@ -15,6 +16,7 @@ async fn healthcheck() -> impl Responder {
     let response = Response {
         message: "Everything is working fine".to_string(),
     };
+    info!("kfwafdf");
     HttpResponse::Ok().json(response)
 }
 
@@ -27,9 +29,10 @@ async fn not_found() -> Result<HttpResponse> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
+    info!("Server is running.");
     let todo_db = repository::database::Database::new();
     let app_data = web::Data::new(todo_db);
-
     HttpServer::new(move ||
         App::new()
             .app_data(app_data.clone())
